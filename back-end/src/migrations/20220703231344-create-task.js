2bfef847508cdb99074d479d294b17368eb74f73
Tainá Santana <tainnaps@gmail.com>
2022-07-03 20:52:22 -0300

    feat: add task model migration

---
+++ back-end/src/migrations/20220703231344-create-task.js
@@ -0,0 +1,27 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Tasks');
+  },
+};
