447e6dfd29ce8f6a88c3662fa49ff0b931854aea
Tainá Santana <tainnaps@gmail.com>
2022-07-05 16:53:43 -0300

    fix: update app import on task test

--- back-end/src/tests/integration/task.test.js
+++ back-end/src/tests/integration/task.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const sinon = require('sinon');
-const app = require('../../../app');
+const app = require('../../app');
 const { Task } = require('../../models');
 const {
   UNORDERED_TASKS,
