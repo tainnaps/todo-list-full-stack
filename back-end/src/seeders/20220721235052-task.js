42ae43581cc553d22f1cba84aa8df42b9fdd1ec9
Tainá Santana <tainnaps@gmail.com>
2022-07-22 15:10:01 -0300

    refactor: add user id on tasks seeders

--- back-end/src/seeders/20220721235052-task.js
+++ back-end/src/seeders/20220721235052-task.js
@@ -1,24 +1,27 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert(
-      'Tasks',
+      'tasks',
       [
         {
           id: 1,
           name: 'Call to the manager',
           status: 'Pending',
+          user_id: 1,
           created_at: new Date(),
         },
         {
           id: 2,
           name: 'Meeting with the Marketing team',
           status: 'Done',
+          user_id: 2,
           created_at: new Date(),
         },
         {
           id: 3,
           name: 'Write performance report of the semester',
           status: 'In progress',
+          user_id: 1,
           created_at: new Date(),
         },
       ],
@@ -27,6 +30,6 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    await queryInterface.bulkDelete('Tasks', null, {});
+    await queryInterface.bulkDelete('tasks', null, {});
   },
 };
