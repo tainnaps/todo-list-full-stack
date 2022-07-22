d508157e42dafdd0169853978a4c081e9ae37961
Tainá Santana <tainnaps@gmail.com>
2022-07-22 17:04:36 -0300

    fix: set tables names on models

--- back-end/src/models/task.js
+++ back-end/src/models/task.js
@@ -31,6 +31,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       timestamps: true,
       updatedAt: false,
+      tableName: 'tasks',
     },
   );
 
--- back-end/src/models/user.js
+++ back-end/src/models/user.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       timestamps: false,
+      tableName: 'users',
     },
   );
 
