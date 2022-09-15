d7f6d13c11683a709a116b6007778cdcb9207a89
Tainá Santana <tainnaps@gmail.com>
2022-09-15 20:05:07 -0300

    refactor: add userId to tasks mocks and delete unused

--- back-end/src/tests/mocks/task.js
+++ back-end/src/tests/mocks/task.js
@@ -1,65 +1,23 @@
 const UNORDERED_TASKS = [
   {
     id: 1,
+    userId: 1,
     name: 'First task',
     status: 'Pending',
     createdAt: '2022-07-04T02:01:17.000Z',
   },
   {
     id: 2,
+    userId: 1,
     name: 'Second task',
     status: 'Done',
-    createdAt: '2022-07-04T02:01:17.000Z',
-  },
-];
-
-const ORDERED_TASKS_BY_NAME_ASC = [
-  {
-    id: 2,
-    name: 'Second task',
-    status: 'Done',
-    createdAt: '2022-07-04T02:01:17.000Z',
-  },
-  {
-    id: 1,
-    name: 'First task',
-    status: 'Pending',
-    createdAt: '2022-07-04T02:01:17.000Z',
-  },
-];
-
-const ORDERED_TASKS_BY_STATUS_ASC = [
-  {
-    id: 1,
-    name: 'First task',
-    status: 'Pending',
-    createdAt: '2022-07-04T02:01:17.000Z',
-  },
-  {
-    id: 2,
-    name: 'Second task',
-    status: 'Done',
-    createdAt: '2022-07-04T02:01:17.000Z',
-  },
-];
-
-const ORDERED_TASKS_BY_DATE_DESC = [
-  {
-    id: 2,
-    name: 'Second task',
-    status: 'Done',
-    createdAt: '2022-07-04T04:09:32.000Z',
-  },
-  {
-    id: 1,
-    name: 'First task',
-    status: 'Pending',
     createdAt: '2022-07-04T02:01:17.000Z',
   },
 ];
 
 const FIFTH_TASK = {
   id: 5,
+  userId: 1,
   name: 'Fifth task',
   status: 'In progress',
   createdAt: '2022-07-04T04:09:32.000Z',
@@ -67,6 +25,7 @@ const FIFTH_TASK = {
 
 const UPDATED_FIFTH_TASK = {
   id: 5,
+  userId: 1,
   name: 'Fifth task',
   status: 'Done',
   createdAt: '2022-07-04T04:09:32.000Z',
@@ -74,9 +33,6 @@ const UPDATED_FIFTH_TASK = {
 
 module.exports = {
   UNORDERED_TASKS,
-  ORDERED_TASKS_BY_NAME_ASC,
-  ORDERED_TASKS_BY_STATUS_ASC,
-  ORDERED_TASKS_BY_DATE_DESC,
   FIFTH_TASK,
   UPDATED_FIFTH_TASK,
 };
