e3947c9782bda020f5a439035198da1cd6f636a7
Tainá Santana <tainnaps@gmail.com>
2022-09-14 21:22:28 -0300

    refactor: add joi error validation on error middleware

--- back-end/src/middlewares/index.js
+++ back-end/src/middlewares/index.js
@@ -1,9 +1,11 @@
+const authMiddleware = require('./auth');
 const errorMiddleware = require('./error');
 const TaskMiddlewares = require('./task');
 const UserMiddlewares = require('./user');
 
 module.exports = {
   errorMiddleware,
+  authMiddleware,
   TaskMiddlewares,
   UserMiddlewares,
 };
--- back-end/src/middlewares/task.js
+++ back-end/src/middlewares/task.js
@@ -1,16 +1,10 @@
 const { TaskSchemas } = require('../schemas');
-const { getCustomError } = require('../helpers');
 
 const validateName = (req, _res, next) => {
   const { name } = req.body;
   const { error } = TaskSchemas.name.validate(name);
 
-  if (error) {
-    const { message } = error.details[0];
-    const customError = getCustomError(message, 400);
-
-    return next(customError);
-  }
+  if (error) return next(error);
 
   return next();
 };
@@ -19,12 +13,7 @@ const validateStatus = (req, _res, next) => {
   const { status } = req.body;
   const { error } = TaskSchemas.status.validate(status);
 
-  if (error) {
-    const { message } = error.details[0];
-    const customError = getCustomError(message, 400);
-
-    return next(customError);
-  }
+  if (error) return next(error);
 
   return next();
 };
