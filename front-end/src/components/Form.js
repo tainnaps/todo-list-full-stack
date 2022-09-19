85a790c9c1c1c91997164b65ecbe719c250441e3
Tainá Santana <tainnaps@gmail.com>
2022-09-19 11:27:20 -0300

    feat: add styled form

---
+++ front-end/src/components/Form.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4vh;
+  width: 100%;
+`;
+
+export default Form;
