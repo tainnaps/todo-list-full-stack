38a6d41398d2071d580af51b5fac82c46ad479f5
Tainá Santana <tainnaps@gmail.com>
2022-09-19 11:28:29 -0300

    feat: add styled content containers

---
+++ front-end/src/components/AppContainer.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const AppContainer = styled.main`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  background-color: #211b15;
+`;
+
+export default AppContainer;
---
+++ front-end/src/components/PageContainer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const PageContainer = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 3vh;
+`;
+
+export default PageContainer;
