ded31f5a80478af4a0e665f4e7fba80979d16151
Tainá Santana <tainnaps@gmail.com>
2022-09-19 11:30:53 -0300

    feat: add login and register routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import AppContainer from './components/AppContainer';
+import Login from './pages/Login';
+import Register from './pages/Register';
+// import './App.css';
 
 function App() {
   return (
-    <div>{process.env.REACT_APP_BACKEND_PORT}</div>
+    <AppContainer>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </AppContainer>
   );
 }
 
