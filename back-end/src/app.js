b062f6eaf488ac9290d1f7f386349de6cdd13c4d
Tainá Santana <tainnaps@gmail.com>
2022-09-19 11:22:43 -0300

    feat: add cors to api

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const { errorMiddleware } = require('./middlewares');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
