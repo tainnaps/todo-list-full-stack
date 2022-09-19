80052fbc242edac34f32fece1b084f519707f6b5
Tainá Santana <tainnaps@gmail.com>
2022-09-19 11:27:29 -0300

    feat: add styled input

---
+++ front-end/src/components/Input.js
@@ -0,0 +1,42 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  padding: 1.5vh 0.7%;
+  width: 25%;
+  border: none;
+  border-bottom: 2px solid #eead2d;
+  background-color: #211b15;
+  color: #cccdcc;
+  font-size: medium;
+  /* border-radius: 4.5px; */
+`;
+
+// function Input(props) {
+//   const {
+//     placeholder, value, type, onChange,
+//   } = props;
+
+//   return (
+//     <StyledInput
+//       placeholder={placeholder}
+//       type={type}
+//       value={value}
+//       onChange={onChange}
+//       required
+//     />
+//   );
+// }
+
+// Input.defaultProps = {
+//   type: 'text',
+// };
+
+// Input.propTypes = {
+//   placeholder: PropTypes.string,
+//   value: PropTypes.string,
+//   onChange: PropTypes.func,
+// }.isRequired;
+
+export default Input;
