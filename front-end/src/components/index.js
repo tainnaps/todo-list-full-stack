b1bf6637d329b46298300d7cf8d8e0c4f0238ce8
Tainá Santana <tainnaps@gmail.com>
2022-09-20 18:40:08 -0300

    feat: add index file for components

---
+++ front-end/src/components/index.js
@@ -0,0 +1,19 @@
+import AppContainer from './AppContainer';
+import PageContainer from './PageContainer';
+import Button from './Button';
+import Form from './Form';
+import Input from './Input';
+import Link from './Link';
+import Title from './Title';
+import Warning from './Warning';
+
+export {
+  AppContainer,
+  PageContainer,
+  Button,
+  Form,
+  Input,
+  Link,
+  Title,
+  Warning,
+};
