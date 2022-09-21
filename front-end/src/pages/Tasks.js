e142b1f755591e63b64c4aa430dc29a7cc60cf07
Tainá Santana <tainnaps@gmail.com>
2022-09-21 01:03:32 -0300

    feat: add authentication on tasks page

--- front-end/src/pages/Tasks.js
+++ front-end/src/pages/Tasks.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import PageContainer from '../components/PageContainer';
-import Title from '../components/Title';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PageContainer, Title } from '../components';
+import { LOCAL_STORAGE_KEY, getItem, removeItem } from '../services/localStorage';
+import request from '../services/request';
 
 function Tasks() {
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [tasks, setTasks] = useState([]);
+
+  const getAllTasks = async (token) => {
+    const { error, data } = await request({
+      method: 'get',
+      url: '/tasks',
+      headers: { Authorization: token },
+    });
+
+    if (error && data.message === 'Unauthorized') {
+      removeItem(LOCAL_STORAGE_KEY);
+      navigate('/');
+    } else {
+      setTasks(data);
+    }
+  };
+
+  useEffect(() => {
+    const authenticate = async () => {
+      const { token } = getItem(LOCAL_STORAGE_KEY);
+
+      if (!token) {
+        navigate('/');
+      } else {
+        await getAllTasks(token);
+      }
+    };
+
+    authenticate();
+  }, []);
+
   return (
     <PageContainer>
       <Title>Tasks</Title>
