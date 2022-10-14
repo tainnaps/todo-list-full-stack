4f48bb2f5506c97e9ff8e817a45c35c263141639
Tainá Santana <tainnaps@gmail.com>
2022-10-14 06:05:01 -0300

    feat: add edit and delete functionalities

--- front-end/src/components/TableBody.js
+++ front-end/src/components/TableBody.js
@@ -1,44 +1,76 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
 import { FaTrash } from 'react-icons/fa';
 import { MdEdit } from 'react-icons/md';
+import { TasksContext } from '../context/Tasks';
+import { getItem, LOCAL_STORAGE_KEY } from '../services/localStorage';
+import request from '../services/request';
 import { TableButton } from '../styled';
 
-function TableBody({ tasks }) {
+function TableBody() {
+  const {
+    tasks,
+    removeTask,
+    saveErrorMessage,
+    setIsEditing,
+    setEditingTask,
+  } = useContext(TasksContext);
+
+  const deleteTask = async (id) => {
+    const { token } = getItem(LOCAL_STORAGE_KEY);
+    const { error, data } = await request({
+      method: 'delete',
+      url: `/tasks/${id}`,
+      headers: { Authorization: token },
+    });
+
+    if (error) {
+      saveErrorMessage(data.message);
+    } else {
+      removeTask(id);
+    }
+  };
+
+  const startEditingTask = async (task) => {
+    setIsEditing(true);
+    setEditingTask(task);
+  };
+
   return (
     <tbody>
-      {tasks.map((task, index) => (
+      { tasks.map((task, index) => (
         <tr key={task.id}>
           <td>
-            {index + 1}
+            { index + 1 }
           </td>
           <td>
-            {task.name}
+            { task.name }
           </td>
           <td>
-            {task.status}
+            { task.status }
           </td>
           <td>
-            {task.createdAt.slice(0, 10)}
+            { task.createdAt.slice(0, 10) }
           </td>
           <td>
-            <TableButton>
+            <TableButton
+              type="button"
+              onClick={() => startEditingTask(task)}
+            >
               <MdEdit />
             </TableButton>
           </td>
           <td>
-            <TableButton>
+            <TableButton
+              type="button"
+              onClick={() => deleteTask(task.id)}
+            >
               <FaTrash />
             </TableButton>
           </td>
         </tr>
-      ))}
+      )) }
     </tbody>
   );
 }
-
-TableBody.propTypes = {
-  tasks: PropTypes.arrayOf(PropTypes.instanceOf('Object')),
-}.isRequired;
 
 export default TableBody;
